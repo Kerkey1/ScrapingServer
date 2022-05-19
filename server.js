c1648b36ec4402c0cc8803062fb81b9e752f3996
Limitless <Alebrium@gmail.com>
2022-05-19 20:58:53 +0300

    change back. Create header and result json files. Reverse entries for POST request

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const bodyParser = require('body-parser');
+let lastData = [];
+let JSONdata = [];
+
 
 const corsOpts = {
     origin: '*',
@@ -20,18 +23,19 @@ app.use(express.urlencoded({
     extended: true
 }));
 
+
 const loadData = (path) => {
     try {
-        return fs.readFileSync(path, 'utf8')
+        return fs.readFileSync(path, 'utf8');
     } catch (err) {
         console.error(err)
         return false
     }
 }
 
-const storeData = (data, path) => {
+const storeData = (data, path, mode) => {
     try {
-        fs.writeFileSync(path, JSON.stringify(data))
+        fs.writeFileSync(path, JSON.stringify(data), { flag: mode })
     } catch (err) {
         console.error(err)
     }
@@ -39,18 +43,44 @@ const storeData = (data, path) => {
 
 
 app.get("/getJSON", (req, res) => {
-    let result = loadData("./result/result.json")
+    let result = loadData("./result/heading.json");
     res.json(JSON.parse(result));
 });
 
+
 app.post("/postJSON", function (request, response) {
     if (!request.body) return response.sendStatus(400);
-    let temp = request.body
-    console.log(temp);
-    storeData(request.body, "./result/result.json")
+    let reverse_array = request.body.reverse();
+
+    for (const element of reverse_array) {
+        lastData.push(element.link);
+        if (lastData.length > 100)
+            lastData.shift();
+    }
+
+    storeData(reverse_array, "./result/result.json", "a");
+    storeData(lastData, "./result/heading.json", "w");
     response.send(request.body);
 });
 
 app.listen(PORT, () => {
+    fs.access("./result/result.json", fs.constants.F_OK, (error) => {
+        if (error) {
+            console.log('result file created');
+            fs.writeFileSync("./result/result.json", '', { flag: 'wx' });
+            fs.writeFileSync("./result/heading.json", '[]', { flag: 'w' });
+        }
+        else {
+            console.log('processing last data...');
+            let JSONdata = fs.readFileSync("./result/result.json");
+            if (JSONdata.length !== 0) {
+                JSONdata = JSON.parse(JSONdata);
+                lastData = JSONdata.slice(-100);
+                lastData.forEach((element, i, lastData) => (lastData[i] = element.link))
+                fs.writeFileSync("./result/heading.json", JSON.stringify(lastData), { flag: 'w' });
+            }
+        }
+        console.log('complete!');
+    });
     console.log(`Server listening on ${PORT}`);
 });
